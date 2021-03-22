29627b030e74f11586e30a09aea41584e4a93c93
laijiantian <872286055@qq.com>
2021-03-22 23:35:19 +0800

    第一次提交

---
+++ zce-test/src/main/java/com/laijiantian/zce/zcetest/ZceTestApplication.java
@@ -0,0 +1,13 @@
+package com.laijiantian.zce.zcetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZceTestApplication.class, args);
+    }
+
+}
