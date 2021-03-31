2c518e7ee5f1714c2e2daae92af337a7e319bf71
laijiantian <872286055@qq.com>
2021-03-31 08:33:17 +0800

    2021-3-31

---
+++ zce-third-party/src/main/java/com/laijiantian/zce/thirdparty/ZceThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package com.laijiantian.zce.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZceThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZceThirdPartyApplication.class, args);
+    }
+
+}
